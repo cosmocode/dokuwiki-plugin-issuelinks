997760c0d60050f2d1b01ef3031ca8d130674c16
Michael Große <grosse@cosmocode.de>
2018-04-30 16:47:32 +0200

    refactor: move function out of global namespace
    No need for this function in the global scope if it is currently not
    used anywhere else.

--- script/tooltip.js
+++ script/tooltip.js
@@ -5,91 +5,6 @@
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-
-window.addTooltip = function addTooltip(selectorOr$element, url, dataOrDataFunction, complete) {
-    'use strict';
-
-    var serverEndpoint = url || window.DOKU_BASE + 'lib/exe/ajax.php';
-    var DELAY = 300;
-    var HOVER_DETECTION_DELAY = 100;
-    var TOOLTIP_PARENT_CLASS = 'hasTooltip';
-
-    function hoverStart() {
-        var timeOutReference;
-        var payload;
-        var $tooltipDiv;
-        var $element = jQuery(this);
-        $element.addClass('hover');
-        if ($element.hasClass(TOOLTIP_PARENT_CLASS)) {
-            if ($element.data('MMtooltipID')) {
-                $tooltipDiv = jQuery('#' + $element.data('MMtooltipID'));
-                $tooltipDiv.show().position({
-                    my: 'left top',
-                    at: 'left bottom',
-                    of: $element,
-                }).attr('aria-hidden', 'false');
-            }
-            return;
-        }
-        payload = typeof dataOrDataFunction === 'function' ? dataOrDataFunction($element) : dataOrDataFunction;
-        timeOutReference = setTimeout(function getToolTip() {
-            var $div = jQuery('<div class="serverToolTip">')
-                .uniqueId()
-                .mouseleave(function hideTooltip() {
-                    $div.removeClass('hover');
-                    $div.hide().attr('aria-hidden', 'true');
-                })
-                .mouseenter(function allowJSHoverDetection() {
-                    $div.addClass('hover');
-                });
-            $element.addClass(TOOLTIP_PARENT_CLASS);
-            jQuery.get(serverEndpoint, payload).done(function injectTooltip(response) {
-                window.magicMatcherUtil.showAjaxMessages(response);
-                $div.html(response.data);
-                $div.appendTo(jQuery('body'));
-                $div.show().position({
-                    my: 'left top',
-                    at: 'left bottom',
-                    of: $element,
-                });
-                if (!$element.hasClass('hover')) {
-                    $div.hide();
-                }
-                $element.data('MMtooltipID', $div.attr('id'));
-                if (typeof complete === 'function') {
-                    complete($element, $div);
-                }
-            }).fail(function handleFailedState(jqXHR) {
-                window.magicMatcherUtil.showAjaxMessages(jqXHR.responseJSON);
-                $div.remove();
-            });
-        }, DELAY);
-        $element.data('timeOutReference', timeOutReference);
-    }
-
-    function hoverEnd() {
-        var $this = jQuery(this);
-        $this.removeClass('hover');
-        clearTimeout($this.data('timeOutReference'));
-        if ($this.data('MMtooltipID')) {
-            setTimeout(function conditionalHideTooltip() {
-                var $tooltip = jQuery('#' + $this.data('MMtooltipID'));
-                if (!$tooltip.hasClass('hover')) {
-                    $tooltip.hide().attr('aria-hidden', 'true');
-                }
-            }, HOVER_DETECTION_DELAY);
-        }
-    }
-
-    if (typeof selectorOr$element === 'string') {
-        jQuery(document).on('mouseenter', selectorOr$element, hoverStart);
-        jQuery(document).on('mouseleave', selectorOr$element, hoverEnd);
-    } else {
-        selectorOr$element.hover(hoverStart, hoverEnd);
-    }
-};
-
-
 jQuery(function initializeTooltips() {
     'use strict';
 
@@ -105,7 +20,90 @@ jQuery(function initializeTooltips() {
         };
     }
 
-    window.addTooltip('.issuelink', undefined, getLinkData, function getAdditionalIssueData($issueLink, $tooltip) {
+    function addTooltip(selectorOr$element, url, dataOrDataFunction, complete) {
+        'use strict';
+
+        var serverEndpoint = url || window.DOKU_BASE + 'lib/exe/ajax.php';
+        var DELAY = 300;
+        var HOVER_DETECTION_DELAY = 100;
+        var TOOLTIP_PARENT_CLASS = 'hasTooltip';
+
+        function hoverStart() {
+            var timeOutReference;
+            var payload;
+            var $tooltipDiv;
+            var $element = jQuery(this);
+            $element.addClass('hover');
+            if ($element.hasClass(TOOLTIP_PARENT_CLASS)) {
+                if ($element.data('MMtooltipID')) {
+                    $tooltipDiv = jQuery('#' + $element.data('MMtooltipID'));
+                    $tooltipDiv.show().position({
+                        my: 'left top',
+                        at: 'left bottom',
+                        of: $element,
+                    }).attr('aria-hidden', 'false');
+                }
+                return;
+            }
+            payload = typeof dataOrDataFunction === 'function' ? dataOrDataFunction($element) : dataOrDataFunction;
+            timeOutReference = setTimeout(function getToolTip() {
+                var $div = jQuery('<div class="serverToolTip">')
+                    .uniqueId()
+                    .mouseleave(function hideTooltip() {
+                        $div.removeClass('hover');
+                        $div.hide().attr('aria-hidden', 'true');
+                    })
+                    .mouseenter(function allowJSHoverDetection() {
+                        $div.addClass('hover');
+                    });
+                $element.addClass(TOOLTIP_PARENT_CLASS);
+                jQuery.get(serverEndpoint, payload).done(function injectTooltip(response) {
+                    window.magicMatcherUtil.showAjaxMessages(response);
+                    $div.html(response.data);
+                    $div.appendTo(jQuery('body'));
+                    $div.show().position({
+                        my: 'left top',
+                        at: 'left bottom',
+                        of: $element,
+                    });
+                    if (!$element.hasClass('hover')) {
+                        $div.hide();
+                    }
+                    $element.data('MMtooltipID', $div.attr('id'));
+                    if (typeof complete === 'function') {
+                        complete($element, $div);
+                    }
+                }).fail(function handleFailedState(jqXHR) {
+                    window.magicMatcherUtil.showAjaxMessages(jqXHR.responseJSON);
+                    $div.remove();
+                });
+            }, DELAY);
+            $element.data('timeOutReference', timeOutReference);
+        }
+
+        function hoverEnd() {
+            var $this = jQuery(this);
+            $this.removeClass('hover');
+            clearTimeout($this.data('timeOutReference'));
+            if ($this.data('MMtooltipID')) {
+                setTimeout(function conditionalHideTooltip() {
+                    var $tooltip = jQuery('#' + $this.data('MMtooltipID'));
+                    if (!$tooltip.hasClass('hover')) {
+                        $tooltip.hide().attr('aria-hidden', 'true');
+                    }
+                }, HOVER_DETECTION_DELAY);
+            }
+        }
+
+        if (typeof selectorOr$element === 'string') {
+            jQuery(document).on('mouseenter', selectorOr$element, hoverStart);
+            jQuery(document).on('mouseleave', selectorOr$element, hoverEnd);
+        } else {
+            selectorOr$element.hover(hoverStart, hoverEnd);
+        }
+    }
+
+    addTooltip('.issuelink', undefined, getLinkData, function getAdditionalIssueData($issueLink, $tooltip) {
         jQuery.get(window.DOKU_BASE + 'lib/exe/ajax.php', {
             'issuelinks-service': $issueLink.data('service'),
             'issuelinks-project': $issueLink.data('project'),
