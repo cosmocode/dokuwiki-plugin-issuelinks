dd3b7ed1ded7103d708252920d89ef575258dd0f
Michael Große <grosse@cosmocode.de>
2018-04-30 16:38:44 +0200

    refactor: remove unused js code

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/* DOKUWIKI:include script/jQueryRotate.js */
-/* DOKUWIKI:include_once lib/jquery.chosen.js */
-/* DOKUWIKI:include script/import.js */
 /* DOKUWIKI:include script/admin_repo.js */
-/* DOKUWIKI:include script/toolbar.js */
 /* DOKUWIKI:include script/tooltip.js */
 /* DOKUWIKI:include script/header.js */
